546062c6a70e3d0aaf9368bf5b7a36404ffe3cad
doowee0509 <ngocduy000937@gmail.com>
2023-05-09 19:38:14 -0400

    added server.js

--- capstone-api/server.js
+++ capstone-api/server.js
@@ -2,5 +2,5 @@ require("dotenv").config()
 const app=require("./app")
 
 app.listen(Number(process.env.PORT) || 3001, () => {
-    console.log(`🚀 Server listening on port ` + PORT)
+    console.log(`🚀 Server listening on port ` + Number(process.env.PORT))
 })
