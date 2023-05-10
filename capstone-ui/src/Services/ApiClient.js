cf33232219ed9270a14e5d7a887e7ade907a2870
doowee0509 <ngocduy000937@gmail.com>
2023-05-09 23:20:02 -0400

    changed production api url

--- capstone-ui/src/Services/ApiClient.js
+++ capstone-ui/src/Services/ApiClient.js
@@ -188,5 +188,5 @@ class ApiClient {
     }
 }
 
-export default new ApiClient(DEVELOPMENT_API_URL)
-// export default new ApiClient(PRODUCTION_API_URL)
+// export default new ApiClient(DEVELOPMENT_API_URL)
+export default new ApiClient(PRODUCTION_API_URL)
--- capstone-ui/src/constants.js
+++ capstone-ui/src/constants.js
@@ -1,2 +1,2 @@
 export const DEVELOPMENT_API_URL = "http://localhost:3001";
-export const PRODUCTION_API_URL = "https://reciholic-backend.herokuapp.com";
+export const PRODUCTION_API_URL = "https://ill-houndstooth-elk.cyclic.app";
